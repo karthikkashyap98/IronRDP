612d20b58afd81835408e4ef8bfe740974d2809d
Karthik Kashyap (KK) <kkashyap@cloudflare.com>
2026-03-31 18:18:02 -0500

    refactor(iron-replay-player): export ReplayDataSource types, remove fetch internals

--- web-client/iron-replay-player/src/main.ts
+++ web-client/iron-replay-player/src/main.ts
@@ -6,8 +6,4 @@ export type { ReplayModule, WasmReplayInstance, RenderResult } from './interface
 export type { PlayerApi } from './interfaces/PlayerApi.js';
 export type { PlaybackState } from './interfaces/PlaybackState.js';
 export type { LoadState } from './interfaces/LoadState.js';
-export type { PlayerFetchError } from './interfaces/PlayerFetchError.js';
-
-// Recording fetch utilities (for consumers who want to prefetch metadata)
-export { fetchHeader, fetchIndexTable, FetchHttpError } from './services/fetchRecording.js';
-export type { Header, IndexTableRow, FetchOptions } from './services/fetchRecording.js';
+export type { ReplayDataSource, ReplayMetadata, ReplayPdu, PlayerError } from './interfaces/ReplayDataSource.js';
